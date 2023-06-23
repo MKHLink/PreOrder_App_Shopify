db0ce84988fdd0b9699805305666f08ea36118e1
MKHLink <likhonhasankomol@gmail.com>
2023-06-23 16:11:38 -0400

    added a text forntend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+const nodemailer = require('nodemailer');
+
 app.get('/',function(req,res){
     res.send('Server up')
 })
